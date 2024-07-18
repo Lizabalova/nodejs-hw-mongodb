830f3af2ef2f88288f9644fbc13391340e029a93
Lizabalova <148805017+Lizabalova@users.noreply.github.com>
2024-07-18 09:42:13 +0200

    Update contacts.js

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,40 +1,20 @@
-import { ContactsCollection } from '../../src/db/models/contacts.js';
+import { Contact } from '../db/contact.js';
+import mongoose from 'mongoose';
 
 export const getAllContacts = async () => {
-  const contacts = await ContactsCollection.find();
+  const contacts = await Contact.find();
   return contacts;
 };
 
-export const getContactById = async (contactId) => {
-  const contact = await ContactsCollection.findById(contactId);
-  return contact;
-};
-
-export const createContact = async (payload) => {
-  const contact = await ContactsCollection.create(payload);
-  return contact;
-};
-
-export const deleteContact = async (contactId) => {
-  const contact = await ContactsCollection.findOneAndDelete({ _id: contactId });
-  return contact;
-};
-
-export const updateContact = async (contactId, payload, options = {}) => {
-  const rawResult = await ContactsCollection.findOneAndUpdate(
-    { _id: contactId },
-    payload,
-    {
-      new: true,
-      includeResultMetadata: true,
-      ...options,
-    },
-  );
-
-  if (!rawResult || !rawResult.value) return null;
-
-  return {
-    contact: rawResult.value,
-    isNew: Boolean(rawResult?.lastErrorObject?.upserted),
-  };
+export const getContactById = async (id) => {
+  try {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return null;
+    }
+    const contact = await Contact.findById(id);
+    return contact;
+  } catch (error) {
+    console.error('Error getting contact by ID:', error);
+    throw error;
+  }
 };
