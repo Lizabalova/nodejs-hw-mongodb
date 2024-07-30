85edb31bc5aeab640162c6d501e2c28f85fd1618
Lizabalova <148805017+Lizabalova@users.noreply.github.com>
2024-07-30 10:58:13 +0200

    Update contacts.js

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -2,12 +2,31 @@ import { Router } from 'express';
 import {
   getContactsController,
   getContactByIdController,
+  createContactController,
+  deleteContactController,
+  patchContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import {
+  createContactSchema,
+  updateContactSchema,
+} from '../validation/contacts.js';
 
 const router = Router();
 
 router.get('/contacts', ctrlWrapper(getContactsController));
 router.get('/contacts/:id', ctrlWrapper(getContactByIdController));
+router.post(
+  '/contacts',
+  validateBody(createContactSchema),
+  ctrlWrapper(createContactController),
+);
+router.delete('/contacts/:id', ctrlWrapper(deleteContactController));
+router.patch(
+  '/contacts/:id',
+  validateBody(updateContactSchema),
+  ctrlWrapper(patchContactController),
+);
 
 export default router;
