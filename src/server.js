fa8c99bf13394118503ee0e4e357304ef6a9ed70
Lizabalova <148805017+Lizabalova@users.noreply.github.com>
2024-07-30 11:00:38 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,7 +3,8 @@ import pino from 'pino-http';
 import cors from 'cors';
 import { env } from './utils/env.js';
 import contactsRouter from './routers/contacts.js';
-import { errorHandler, notFoundHandler } from './middlewares/errorHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
 export const setupServer = () => {
   const app = express();
