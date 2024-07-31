370f70414a52703d0f945fc9ec942891c22ff67f
Lizabalova <148805017+Lizabalova@users.noreply.github.com>
2024-07-31 10:44:19 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,44 +2,36 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { env } from './utils/env.js';
-import contactsRouter from './routers/contacts.js';
+import router from './routers/contacts.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
-export const setupServer = () => {
-  const app = express();
-  // const PORT = 3000(process.env.PORT);
-  const PORT = Number(env('PORT', '3000'));
 
-  app.use(express.json());
-  app.use(cors());
+const PORT = Number(env('PORT', '3000'));
 
-  app.use(
-    pino({
-      transport: {
-        target: 'pino-pretty',
-      },
-    }),
-  );
+export const setupServe = () => {
+    const app = express();
 
-  app.use((req, res, next) => {
-    console.log(`Time: ${new Date().toLocaleString()}`);
-    next();
-  });
+    app.use(express.json());
+    app.use(cors());
 
-  app.get('/', (req, res) => {
-    res.json({
-      message: 'Hello world!',
+    app.use(
+        pino({
+            transport: {
+                target: 'pino-pretty',
+            },
+        }),
+    );
+
+    app.use(router);
+
+
+    app.use('*', notFoundHandler);
+
+    app.use(errorHandler);
+
+
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
     });
-  });
-
-  app.use(contactsRouter);
-
-  app.use('*', notFoundHandler);
-
-  app.use(errorHandler);
-
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
 };
