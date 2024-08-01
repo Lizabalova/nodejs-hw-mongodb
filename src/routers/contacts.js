66c47e821407d03364eb1ebc58dba074b655a767
Lizabalova <148805017+Lizabalova@users.noreply.github.com>
2024-08-01 08:32:31 +0200

    Update contacts.js

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,13 +1,22 @@
 import { Router } from 'express';
-import { getContactsByIdController, getContactsController, createContactController, deleteContactController, patchContactController } from '../controllers/contacts.js';
+import {
+    getContactsByIdController,
+    getContactsController,
+    createContactController,
+    deleteContactController,
+    patchContactController
+} from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { validateBody } from '../middlewares/validateBody.js';
+import { validateObjectId } from '../middlewares/validateObjectId.js'; // Import the new middleware
 import { createContactSchema, updateContactSchema } from '../validation/contacts.js';
 
 const router = Router();
+
 router.get('/contacts', ctrlWrapper(getContactsController));
-router.get('/contacts/:contactId', ctrlWrapper(getContactsByIdController));
-router.patch('/contacts/:contactId', validateBody(updateContactSchema), ctrlWrapper(patchContactController));
+router.get('/contacts/:contactId', validateObjectId, ctrlWrapper(getContactsByIdController));
+router.patch('/contacts/:contactId', validateObjectId, validateBody(updateContactSchema), ctrlWrapper(patchContactController));
 router.post('/contacts', validateBody(createContactSchema), ctrlWrapper(createContactController));
-router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
+router.delete('/contacts/:contactId', validateObjectId, ctrlWrapper(deleteContactController));
+
 export default router;
