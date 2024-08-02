1c4152504e72728f8034099e17d0ef75aebc3bbc
Lizabalova <148805017+Lizabalova@users.noreply.github.com>
2024-08-02 09:59:45 +0200

    Update contacts.js

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,25 +1,17 @@
 import { Router } from 'express';
-import {
-    getContactsByIdController,
-    getContactsController,
-    createContactController,
-    deleteContactController,
-    patchContactController
-} from '../controllers/contacts.js';
+import { getContactsByIdController, getContactsController, createContactController, deleteContactController, patchContactController } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { validateBody } from '../middlewares/validateBody.js';
-import { validateObjectId } from '../middlewares/contactId.js'; // Import the new middleware
 import { createContactSchema, updateContactSchema } from '../validation/contacts.js';
 import { authenticate } from '../middlewares/authenticate.js';
 
-export const router = Router();
 
-router.use(authenticate);
+export const contactsRouter = Router();
 
-router.get('/contacts', ctrlWrapper(getContactsController));
-router.get('/contacts/:contactId', validateObjectId, ctrlWrapper(getContactsByIdController));
-router.patch('/contacts/:contactId', validateObjectId, validateBody(updateContactSchema), ctrlWrapper(patchContactController));
-router.post('/contacts', validateBody(createContactSchema), ctrlWrapper(createContactController));
-router.delete('/contacts/:contactId', validateObjectId, ctrlWrapper(deleteContactController));
+contactsRouter.use(authenticate);
 
-export default router;
+contactsRouter.get('/', ctrlWrapper(getContactsController));
+contactsRouter.get('/:contactId', ctrlWrapper(getContactsByIdController));
+contactsRouter.patch('/:contactId', validateBody(updateContactSchema), ctrlWrapper(patchContactController));
+contactsRouter.post('/', validateBody(createContactSchema), ctrlWrapper(createContactController));
+contactsRouter.delete('/:contactId', ctrlWrapper(deleteContactController));
