d29e25619ea6a0a32d66d1817a5335285975a441
Lizabalova <148805017+Lizabalova@users.noreply.github.com>
2024-08-05 09:15:21 +0200

    Add files via upload

---
+++ src/routes/contacts.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import {
+  createContactController,
+  deleteContactController,
+  getContactByIdController,
+  getContactsController,
+  patchContactController,
+} from '../controllers/contacts.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import {
+  createContactSchema,
+  updateContactSchema,
+} from '../validation/contacts.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { isValidId } from '../middlewares/isValidId.js';
+import { authenticate } from '../middlewares/authenticate.js';
+
+const router = express.Router();
+const parseJSON = express.json({
+  type: ['application/json', 'application/vnd.api+json'],
+  limit: '100kb',
+});
+
+router.use(authenticate);
+
+router.get('/', ctrlWrapper(getContactsController));
+
+router.get('/:contactId', isValidId, ctrlWrapper(getContactByIdController));
+
+router.post(
+  '',
+  parseJSON,
+  validateBody(createContactSchema),
+  ctrlWrapper(createContactController),
+);
+
+router.delete('/:contactId', isValidId, ctrlWrapper(deleteContactController));
+
+router.patch(
+  '/:contactId',
+  parseJSON,
+  isValidId,
+  validateBody(updateContactSchema),
+  ctrlWrapper(patchContactController),
+);
+
+export default router;
---
+++ src/server.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+
+import { env } from './utils/env.js';
+import router from './routes/index.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import cookieParser from 'cookie-parser';
+
+const PORT = Number(env('PORT', '3000'));
+
+export const startServer = () => {
+  const app = express();
+
+  app.use(cors());
+
+  app.use(cookieParser());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.use(router);
+
+  app.use('*', notFoundHandler);
+
+  app.use(errorHandler);
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
