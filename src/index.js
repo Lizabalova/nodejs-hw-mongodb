79497fe46f843f842f763e1ef034bf2088e1f6fb
Lizabalova <148805017+Lizabalova@users.noreply.github.com>
2024-08-05 09:14:48 +0200

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import { initMongoConnection } from './db/initMongoConnection.js';
-import { setupServe } from './server.js';
-
-const bootstrap = async () => {
-    await initMongoConnection();
-    setupServe();
-};
-
-bootstrap();
--- src/utils/error.js
+++
@@ -1,5 +0,0 @@
-export const throwNotFoundError = () => {
-    const error = new Error('Not Found');
-    error.status = 404;
-    throw error;
-};
